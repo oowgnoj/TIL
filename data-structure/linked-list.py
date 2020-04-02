ddf9b6ee7050fee8330a1cae7b3e2ef99cbc865d
oowgnoj <mangtaob@gmail.com>
2020-04-03 08:28:21 +0900

    add linked list

---
+++ data-structure/linked-list.py
@@ -0,0 +1,34 @@
+class Node:
+	"""링크드 리스트의 노드 클래스"""
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+
+
+
+# class LinkedList:
+# 	# method required 
+# 	# 	2. append
+# 	# 	3. delete last node
+# 	# 	4. delete first node
+# 	#	5. delete specific node
+
+# 	# property required
+# 	# 	1. node
+# 	# 		1. head	
+# 	# 		2. next
+# 	head = None
+
+# 	def __init__(self, headData):
+# 		self.headData = headData
+# 		self.head = {'data': headData, next: None}
+
+# 	def create_node(self, data):
+# 			return {'data' : data, next: None}
+
+	
+
+# test = LinkedList(3)
+
+# print(LinkedList.head)
